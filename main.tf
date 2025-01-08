aa18907cde5729206cf821ee330097f66f60b474
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-08 22:25:21 +0530

    run all components

--- main.tf
+++ main.tf
@@ -1,21 +1,25 @@
-# module "frontend" {
-#   depends_on = [module.backend]
-#   source = "./modules/app"
-#   component = "frontend"
-#   instance_type=var.instance_type
-#   env = var.env
-#   zone_id     = var.zone_id
-#   vault_token = var.vault_token
-# }
-# module "backend" {
-#   depends_on = [module.mysql]
-#   source = "./modules/app"
-#   component = "backend"
-#   instance_type=var.instance_type
-#   env = var.env
-#   zone_id = var.zone_id
-#   vault_token = var.vault_token
-# }
+module "frontend" {
+  depends_on = [module.backend]
+  source = "./modules/app"
+  component = "frontend"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id     = var.zone_id
+  vault_token = var.vault_token
+  subnets_id = module.vpc.frontend_subnets
+  vpc_id = module.vpc.vpc_id
+}
+module "backend" {
+  depends_on = [module.mysql]
+  source = "./modules/app"
+  component = "backend"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+  subnets_id = module.vpc.backend_subnets
+  vpc_id = module.vpc.vpc_id
+}
 module "mysql" {
   source = "./modules/app"
   component = "mysql"
