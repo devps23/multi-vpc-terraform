23dcf357883956de7f48236e817aa4afcf839c58
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-08 22:07:14 +0530

    quotations

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -27,13 +27,13 @@ resource "aws_security_group" "allow_tls" {
     from_port   =  0
     to_port     =  0
     protocol    = "-1"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   =  0
     to_port     =  0
     protocol    = "-1"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
     Name = "${var.env}-sg"
