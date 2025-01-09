4452f5e85028cc0ea56c37028ccaac23ca595ed0
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 17:02:54 +0530

    to edit default routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -36,10 +36,6 @@ resource "aws_route_table" "public" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.vpc.id
   route {
-    cidr_block = var.default_vpc_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-  }
-  route {
     gateway_id                = aws_internet_gateway.gw.id
     cidr_block                 = "0.0.0.0/0"
   }
@@ -67,12 +63,12 @@ resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.public[count.index].id
 }
-# resource "aws_route" "public" {
-#   count                    = length(var.public_subnets)
-#   route_table_id            = aws_route_table.public[count.index].id
-#   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
+resource "aws_route" "public" {
+  count                    = length(var.public_subnets)
+  route_table_id            = aws_route_table.public[count.index].id
+  destination_cidr_block    = var.default_vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+}
 # create a frontend subnets
 resource "aws_subnet" "frontend_subnets" {
   count     = length(var.frontend_subnets)
@@ -91,10 +87,7 @@ resource "aws_route_table" "frontend" {
     cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
-  route {
-    cidr_block = var.default_vpc_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-  }
+
   tags = {
     Name = "${var.env}-frontend-rt-${count.index}"
   }
@@ -104,12 +97,12 @@ resource "aws_route_table_association" "frontend" {
   subnet_id      = aws_subnet.frontend_subnets[count.index].id
   route_table_id = aws_route_table.frontend[count.index].id
 }
-# resource "aws_route" "frontend" {
-#   count = length(var.frontend_subnets)
-#   route_table_id            = aws_route_table.frontend[count.index].id
-#   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
+resource "aws_route" "frontend" {
+  count = length(var.frontend_subnets)
+  route_table_id            = aws_route_table.frontend[count.index].id
+  destination_cidr_block    = var.default_vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+}
 resource "aws_route" "default" {
   route_table_id            = var.default_route_table_id
   destination_cidr_block    = var.vpc_cidr_block
