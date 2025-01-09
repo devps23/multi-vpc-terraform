ad7e90859dbea9f6bcd56111f53cca62fa48157c
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 17:17:20 +0530

    to edit default routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -106,12 +106,7 @@ resource "aws_route_table_association" "frontend" {
   subnet_id      = aws_subnet.frontend_subnets[count.index].id
   route_table_id = aws_route_table.frontend[count.index].id
 }
-# resource "aws_route" "frontend" {
-#   count = length(var.frontend_subnets)
-#   route_table_id            = aws_vpc.vpc.main_route_table_id[count.index]
-#   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
+
 resource "aws_route" "default" {
   route_table_id            = var.default_route_table_id
   destination_cidr_block    = var.vpc_cidr_block
