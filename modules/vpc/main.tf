ab584d4fe97a4f3eb6400fbe0a8a73f308957528
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 17:46:52 +0530

    git pull

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,192 +1,180 @@
-// create a vpc
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "${var.env}-vpc"
+    Name = "vpc-${var.env}-new"
   }
 }
+resource "aws_subnet" "frontend_subnets" {
+  count                = length(var.frontend_subnets)
+  vpc_id               = aws_vpc.vpc.id
+  cidr_block           = var.frontend_subnets[count.index]
+  availability_zone    = var.availability_zone[count.index]
 
-# peer connection between vpc's
-resource "aws_vpc_peering_connection" "peerconn" {
-  peer_vpc_id   = var.default_vpc_id
-  vpc_id        = aws_vpc.vpc.id
-  auto_accept   = true
   tags = {
-    Name = "${var.env}-vpcpeer"
+    Name = "${var.env}-frontend-subnets-${count.index}"
   }
 }
-# create an internet gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "${var.env}-igw"
-  }
-}
-# create public subnets
+# resource "aws_subnet" "backend_subnets" {
+#   count                = length(var.backend-subnets)
+#   vpc_id               = aws_vpc.vpc.id
+#   cidr_block           = var.backend-subnets[count.index]
+#   availability_zone    = var.availability_zone[count.index]
+#
+#   tags = {
+#     Name = "${var.env}-backend-subnets-${count.index}"
+#   }
+# }
+# resource "aws_subnet" "db_subnets" {
+#   count                = length(var.db-subnets)
+#   vpc_id               = aws_vpc.vpc.id
+#   cidr_block           = var.db-subnets[count.index]
+#   availability_zone    = var.availability_zone[count.index]
+#
+#   tags = {
+#     Name = "${var.env}-db-subnets-${count.index}"
+#   }
+# }
 resource "aws_subnet" "public_subnets" {
-  count      = length(var.public_subnets)
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.public_subnets[count.index]
-  availability_zone = var.availability_zone[count.index]
+  count                = length(var.public_subnets)
+  vpc_id               = aws_vpc.vpc.id
+  cidr_block           = var.public_subnets[count.index]
+  availability_zone    = var.availability_zone[count.index]
+
   tags = {
-    Name = "${var.env}-public-subnet-${count.index+1}"
+    Name = "${var.env}-public-subnets-${count.index}"
   }
 }
-resource "aws_route_table" "public" {
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "ig"
+  }
+}
+resource "aws_route_table" "frontend_route" {
+  count = length(var.frontend_subnets)
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = var.default_vpc_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+
+  }
+  route {
+    cidr_block ="0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+
+  }
+  tags = {
+    Name = "frontend-rt-${count.index}"
+  }
+}
+# resource "aws_route_table" "backend_route" {
+#   count = length(var.backend-subnets)
+#   vpc_id = aws_vpc.vpc.id
+#
+#   route {
+#     cidr_block = var.default_vpc_cidr
+#     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#
+#   }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat[count.index].id
+#
+#   }
+#   tags = {
+#     Name = "backend-rt-${count.index}"
+#   }
+# }
+# resource "aws_route_table" "db_route" {
+#   count = length(var.db-subnets)
+#   vpc_id = aws_vpc.vpc.id
+#
+#   route {
+#     cidr_block = var.default_vpc_cidr
+#     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#
+#   }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat[count.index].id
+#
+#   }
+#   tags = {
+#     Name = "db-rt-${count.index}"
+#   }
+# }
+resource "aws_route_table" "public_route" {
   count = length(var.public_subnets)
   vpc_id = aws_vpc.vpc.id
+
   route {
-    cidr_block                 = "0.0.0.0/0"
-    gateway_id                = aws_internet_gateway.gw.id
+    cidr_block = var.default_vpc_cidr_block
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
   }
-  route{
-    cidr_block = "172.31.0.0/16"
-    vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
-
   tags = {
-    Name = "${var.env}-public-rt-${count.index}"
+    Name = "public-rt-${count.index}"
   }
 }
-# create nat gateway
+resource "aws_eip" "eip" {
+  count = length(var.public_subnets)
+  domain   = "vpc"
+}
 resource "aws_nat_gateway" "nat" {
   count = length(var.public_subnets)
   allocation_id = aws_eip.eip[count.index].id
-  subnet_id     = aws_subnet.public_subnets[count.index].id
-  tags = {
-    Name = "${var.env}-nat"
-  }
-}
-# create eip
-resource "aws_eip" "eip" {
-  count    = length(var.public_subnets)
-  domain   = "vpc"
-}
-resource "aws_route_table_association" "public" {
-  count = length(var.public_subnets)
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
-}
-# resource "aws_route" "public" {
-#   count                    = length(var.public_subnets)
-#   route_table_id            = aws_route_table.public[count.index].id
-#   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
-# create a frontend subnets
-resource "aws_subnet" "frontend_subnets" {
-  count     = length(var.frontend_subnets)
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.frontend_subnets[count.index]
-  availability_zone = var.availability_zone[count.index]
-  tags = {
-    Name = "${var.env}-frontend-subnet-${count.index+1}"
-  }
-}
-# create route table for frontend
-resource "aws_route_table" "frontend" {
-  count = length(var.frontend_subnets)
-  vpc_id = aws_vpc.vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat[count.index].id
-  }
-  route{
-    cidr_block = "172.31.0.0/16"
-    vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-  }
+  subnet_id = aws_subnet.public_subnets[count.index].id
 
   tags = {
-    Name = "${var.env}-frontend-rt-${count.index}"
+    Name = "nat-gw"
   }
 }
-resource "aws_route_table_association" "frontend" {
+resource "aws_route_table_association" "frontend_ass" {
   count = length(var.frontend_subnets)
   subnet_id      = aws_subnet.frontend_subnets[count.index].id
-  route_table_id = aws_route_table.frontend[count.index].id
+  route_table_id = aws_route_table.frontend_route[count.index].id
 }
-
-resource "aws_route" "default" {
-  route_table_id            = var.default_route_table_id
-  destination_cidr_block    = var.vpc_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-}
-# create a backend subnets
-# resource "aws_subnet" "backend_subnets" {
-#   count     = length(var.backend_subnets)
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = var.backend_subnets[count.index]
-#   availability_zone = var.availability_zone[count.index]
-#
-#   tags = {
-#     Name = "${var.env}-backend-subnet-${count.index+1}"
-#   }
-# }
-# # create a backend subnets
-# resource "aws_subnet" "mysql_subnets" {
-#   count     = length(var.mysql_subnets)
-#   vpc_id     = aws_vpc.vpc.id
-#   cidr_block = var.mysql_subnets[count.index]
-#   availability_zone = var.availability_zone[count.index]
-#   tags = {
-#     Name = "${var.env}-mysql-subnet-${count.index+1}"
-#   }
-# }
-
-
-
-# resource "aws_route_table" "backend" {
-#   count = length(var.backend_subnets)
-#   vpc_id = aws_vpc.vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat[count.index].id
-#   }
-#   tags = {
-#     Name = "${var.env}-backend-rt-${count.index}"
-#   }
-# }
-# resource "aws_route_table" "mysql" {
-#   count = length(var.mysql_subnets)
-#   vpc_id = aws_vpc.vpc.id
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     nat_gateway_id = aws_nat_gateway.nat[count.index].id
-#   }
-#   tags = {
-#     Name = "${var.env}-mysql-rt-${count.index}"
-#   }
-# }
-
-
-
-
-
-# resource "aws_route_table_association" "backend" {
-#   count = length(var.public_subnets)
+# resource "aws_route_table_association" "backend_ass" {
+#   count = length(var.backend-subnets)
 #   subnet_id      = aws_subnet.backend_subnets[count.index].id
-#   route_table_id = aws_route_table.backend[count.index].id
+#   route_table_id = aws_route_table.backend_route[count.index].id
 # }
-# resource "aws_route_table_association" "mysql" {
-#   count = length(var.mysql_subnets)
-#   subnet_id      = aws_subnet.mysql_subnets[count.index].id
-#   route_table_id = aws_route_table.mysql[count.index].id
+# resource "aws_route_table_association" "db_ass" {
+#   count = length(var.db-subnets)
+#   subnet_id      = aws_subnet.db_subnets[count.index].id
+#   route_table_id = aws_route_table.db_route[count.index].id
 # }
+resource "aws_route_table_association" "public_ass" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_route[count.index].id
+}
 
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id = var.default_vpc_id
+  vpc_id = aws_vpc.vpc.id
+  auto_accept = true
+  tags = {
+    Name = "peer-${var.env}-new"
+  }
+}
 
-# resource "aws_route" "backend" {
-#   count = length(var.backend_subnets)
-#   route_table_id            = aws_route_table.backend[count.index].id
+# resource "aws_route" "entry_route"{
+#   route_table_id            = aws_vpc.vpc.main_route_table_id
 #   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+#
 # }
-# resource "aws_route" "mysql" {
-#   count = length(var.mysql_subnets)
-#   route_table_id            = aws_route_table.mysql[count.index].id
-#   destination_cidr_block    = var.default_vpc_cidr_block
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
+resource "aws_route" "entry_route_default"{
+  route_table_id            = var.default_route_table_id
+  destination_cidr_block    = aws_vpc.vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
 
-
+}
 
