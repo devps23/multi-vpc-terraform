82384a2ce6b02b37dc3a3a0567d1659ff0821d8c
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 15:28:53 +0530

    create an internet gateway edit public routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -86,10 +86,6 @@ resource "aws_route_table" "public" {
     gateway_id                = aws_internet_gateway.gw.id
     cidr_block                 = "0.0.0.0/0"
   }
-  route {
-    destination_cidr_block    = var.vpc_cidr_block
-    vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-  }
   tags = {
     Name = "${var.env}-public-rt-${count.index}"
   }
@@ -102,37 +98,39 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# create nat gateway
-# resource "aws_nat_gateway" "nat" {
-#   count = length(var.public_subnets)
-#   allocation_id = aws_eip.eip.id
-#   subnet_id     = aws_subnet.public_subnets[count.index].id
-#   tags = {
-#     Name = "${var.env}-nat"
-#   }
-# }
-# # create eip
-#   resource "aws_eip" "eip" {
-#     count    = length(var.public_subnets)
-#     domain   = "${var.env}-eip"
-#   }
-#
-# resource "aws_route" "frontend" {
-#   count                     = length(var.frontend_subnets)
-#   route_table_id            = aws_route_table.frontend[count.index].id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
-# resource "aws_route" "backend" {
-#   count                     = length(var.backend_subnets)
-#   route_table_id            = aws_route_table.backend[count.index].id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
-# resource "aws_route" "mysql" {
-#   count                     = length(var.mysql_subnets)
-#   route_table_id            = aws_route_table.mysql[count.index].id
-#   destination_cidr_block    = "0.0.0.0/0"
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
-# }
+create nat gateway
+resource "aws_nat_gateway" "nat" {
+  count = length(var.public_subnets)
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public_subnets[count.index].id
+  tags = {
+    Name = "${var.env}-nat"
+  }
+}
+# create eip
+  resource "aws_eip" "eip" {
+    count    = length(var.public_subnets)
+    domain   = "${var.env}-eip"
+  }
 
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
+resource "aws_route_table_association" "frontend" {
+  count = length(var.frontend_subnets)
+  subnet_id      = aws_subnet.frontend_subnets[count.index].id
+  route_table_id = aws_route_table.frontend[count.index].id
+}
+resource "aws_route_table_association" "backend" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.backend_subnets[count.index].id
+  route_table_id = aws_route_table.backend[count.index].id
+}
+resource "aws_route_table_association" "mysql" {
+  count = length(var.mysql_subnets)
+  subnet_id      = aws_subnet.mysql_subnets[count.index].id
+  route_table_id = aws_route_table.mysql[count.index].id
+}
