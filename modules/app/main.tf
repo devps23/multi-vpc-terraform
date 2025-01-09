21cf14ebc44aab1760c2466f42dfc88f5011b0e3
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 17:04:07 +0530

    to edit default routes

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -29,9 +29,9 @@ resource "aws_security_group" "sg" {
   description = "${var.env}-vsg-${var.component}"
   vpc_id      = var.vpc_id
   ingress {
-    from_port   =  22
-    to_port     =  22
-    protocol    = "TCP"
+    from_port   =  0
+    to_port     =  0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
