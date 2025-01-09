53de159214d343046b7a3ecea60ae54eed2992d1
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 16:21:06 +0530

    to edit default routes

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -4,9 +4,9 @@ output "vpc_id"{
 output "frontend_subnets" {
   value = aws_subnet.frontend_subnets.*.id
 }
-output "backend_subnets" {
-  value = aws_subnet.backend_subnets.*.id
-}
-output "mysql_subnets" {
-  value = aws_subnet.mysql_subnets.*.id
-}
+# output "backend_subnets" {
+#   value = aws_subnet.backend_subnets.*.id
+# }
+# output "mysql_subnets" {
+#   value = aws_subnet.mysql_subnets.*.id
+# }
