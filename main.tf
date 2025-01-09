9beccab296e625d9f42c305fd1a00c6c498a274f
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 19:48:08 +0530

    create backend and mysql subnets

--- main.tf
+++ main.tf
@@ -9,27 +9,27 @@ module "frontend" {
   subnets_id = module.vpc.frontend_subnets
   vpc_id = module.vpc.vpc_id
 }
-# module "backend" {
-#   depends_on = [module.mysql]
-#   source = "./modules/app"
-#   component = "backend"
-#   instance_type=var.instance_type
-#   env = var.env
-#   zone_id = var.zone_id
-#   vault_token = var.vault_token
-#   subnets_id = module.vpc.backend_subnets
-#   vpc_id = module.vpc.vpc_id
-# }
-# module "mysql" {
-#   source = "./modules/app"
-#   component = "mysql"
-#   instance_type=var.instance_type
-#   env = var.env
-#   zone_id = var.zone_id
-#   vault_token = var.vault_token
-#   vpc_id = module.vpc.vpc_id
-#   subnets_id = module.vpc.mysql_subnets
-# }
+module "backend" {
+  depends_on = [module.mysql]
+  source = "./modules/app"
+  component = "backend"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+  subnets_id = module.vpc.backend_subnets
+  vpc_id = module.vpc.vpc_id
+}
+module "mysql" {
+  source = "./modules/app"
+  component = "mysql"
+  instance_type=var.instance_type
+  env = var.env
+  zone_id = var.zone_id
+  vault_token = var.vault_token
+  vpc_id = module.vpc.vpc_id
+  subnets_id = module.vpc.mysql_subnets
+}
 module "vpc"{
   source = "./modules/vpc"
   vpc_cidr_block = var.vpc_cidr_block
