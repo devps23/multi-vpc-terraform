02e4a01df6456ce787fd588972e7c6ad1520e6fa
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-09 15:10:22 +0530

    create an internet gateway edit public routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -98,5 +98,39 @@ resource "aws_route" "public" {
   route_table_id            = aws_route_table.public[count.index].id
   destination_cidr_block    = "0.0.0.0/0"
   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+  gateway_id                = aws_internet_gateway.gw.id
 }
+# create nat gateway
+# resource "aws_nat_gateway" "nat" {
+#   count = length(var.public_subnets)
+#   allocation_id = aws_eip.eip.id
+#   subnet_id     = aws_subnet.public_subnets[count.index].id
+#   tags = {
+#     Name = "${var.env}-nat"
+#   }
+# }
+# # create eip
+#   resource "aws_eip" "eip" {
+#     count    = length(var.public_subnets)
+#     domain   = "${var.env}-eip"
+#   }
+#
+# resource "aws_route" "frontend" {
+#   count                     = length(var.frontend_subnets)
+#   route_table_id            = aws_route_table.frontend[count.index].id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+# }
+# resource "aws_route" "backend" {
+#   count                     = length(var.backend_subnets)
+#   route_table_id            = aws_route_table.backend[count.index].id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+# }
+# resource "aws_route" "mysql" {
+#   count                     = length(var.mysql_subnets)
+#   route_table_id            = aws_route_table.mysql[count.index].id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   vpc_peering_connection_id = aws_vpc_peering_connection.peerconn.id
+# }
 
